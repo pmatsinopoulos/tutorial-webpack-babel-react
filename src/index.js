19b3b9c24631a04d03be962574ab46bbfc476abf
Panayotis Matsinopoulos <>
2019-07-22 00:57:02 +0300

    Initial commit - WebPack Dev Server setup is working perfect

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FormContainer from './js/components/container/FormContainer.jsx';
+
+ReactDOM.render(
+  <div>
+    <h1>Webpack, React and Dev Server</h1>
+    <FormContainer />
+  </div>  ,
+  document.getElementById('create-article-form')
+)
